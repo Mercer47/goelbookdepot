c692fe36afccfa8fa56c10b9af2bcaa75b7fb09d
Mercer <raghavkumakshay@gmail.com>
2020-07-30 13:12:03 +0530

    improved user interface

--- applications/goelbookdepot/views/user/account.php
+++ applications/goelbookdepot/views/user/account.php
@@ -1,14 +1,14 @@
 <?php $this->view('layouts/user_header') ?>
     <div class="col-xs-12">
         <p class="account-details-heading">Personal Information</p>
-        <p class="account-detail-label">Name</p>
-        <p class="account-detail"><?php echo $details->name; ?></p>
-        <p class="account-detail-label">Address</p>
-        <p class="account-detail"><?php echo $details->address; ?></p>
-        <p class="account-detail-label">Phone</p>
-        <p class="account-detail"><?php echo $details->phone; ?></p>
-        <p class="account-detail-label">Email</p>
-        <p class="account-detail"><?php echo $details->email; ?></p>
+        <p class="form-heading">NAME</p>
+        <input type="text" name="name" class="form-input" value="<?php echo $details->name; ?>"/>
+        <p class="form-heading">ADDRESS</p>
+        <input type="text" class="form-input" name="address" value="<?php echo $details->address; ?>" />
+        <p class="form-heading">PHONE</p>
+        <input type="number" class="form-input" name="phone" value="<?php echo $details->phone; ?>" />
+        <p class="form-heading">EMAIL</p>
+        <input type="email" class="form-input" name="email" value="<?php echo $details->email; ?>" />
         <p class="account-detail-label">Change Password</p>
     </div>
 <?php $this->view('layouts/user_footer') ?>
