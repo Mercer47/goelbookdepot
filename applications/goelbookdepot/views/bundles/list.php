f2dc824e7e35e816ce38b30d868f349f8ab4ec93
Mercer <raghavkumakshay@gmail.com>
2020-08-04 13:52:50 +0530

    Front: updated bundles ui

--- applications/goelbookdepot/views/bundles/list.php
+++ applications/goelbookdepot/views/bundles/list.php
@@ -4,10 +4,6 @@
         <a href="<?php echo site_url('bundle/view/').$bundle->id ?>" style="color: black;">
             <div class="col-md-12 category-container">
                 <?php echo $bundle->name ?>
-                <br/>
-                <?php echo "Only for ₹".$bundle->effective_price." (".$bundle->discount."% off)" ?>
-                <br/>
-                <?php echo $bundle->gift ?>
             </div>
         </a>
     <?php } ?>
--- applications/goelbookdepot/views/bundles/view.php
+++ applications/goelbookdepot/views/bundles/view.php
@@ -2,10 +2,12 @@
 
     <?php foreach(json_decode($bundle->books) as $item) {
         $book = $this->db->get_where('books',array('id' => $item))->first_row(); ?>
-        <div class="col-xs-12">
-            <div class="col-xs-4">
-                <img src="<?php echo base_url('assets/thumbnails/').$book->image ?>" style="width: 100%" />
-            </div>
+        <div class="col-xs-12 bundle-wrapper">
+            <a href="<?php echo site_url('home/showbook/').$book->id ?>">
+                <div class="col-xs-4">
+                    <img src="<?php echo base_url('assets/thumbnails/').$book->image ?>" style="width: 100%" />
+                </div>
+            </a>
             <div class="col-xs-8">
                 <p><?php echo $book->title ?></p>
             </div>
@@ -20,8 +22,16 @@
     <div class="col-xs-12 add-sign">
         =
     </div>
-    <div class="col-xs-12 gift-box">
-        <?php echo "Only for ₹".$bundle->effective_price." (".$bundle->discount."% off)" ?>
+
+    <div class="col-xs-12 gift-box" style="padding-bottom: 30px">
+        <p style="color: #F8DC02">
+            MRP:
+            <?php echo "₹".$bundle->price ?>
+        </p>
+        <p>
+            Your Price:
+            <?php echo "₹".$bundle->effective_price." (".$bundle->discount."% off)" ?>
+        </p>
         <?php if ($bundleAsBook->availability) {?>
             <?php echo form_open(site_url('home/cart'), array('method' => 'POST')) ?>
                 <input type="hidden" name="id" value="<?php echo $bundleAsBook->id; ?>">
