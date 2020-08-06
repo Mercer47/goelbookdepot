a888de972522f86d42218f3ac5a49c49a9720458
Mercer <raghavkumakshay@gmail.com>
2020-08-06 13:10:22 +0530

    update config for production

--- applications/gbdadmin/config/production/config.php
+++ applications/gbdadmin/config/production/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://macmer.in/gbd-test';
+$config['base_url'] = 'https://goelbookdepot.in';
 /*
 |--------------------------------------------------------------------------
 | Index File
